c47ba2522d9660bf2b7097bb3187d29373154def
Kyrylo_Kud <kudkiril@gmail.com>
2025-09-16 16:53:24 +0200

    hw

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hw_26.1_function-component_voting/',
+  base: '/hw_26.1_function_component_voting/',
 })
